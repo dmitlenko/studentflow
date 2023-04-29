6303138af0616176da5861d50753d70539f477e6
Denys Mitlenko <softonka@gmail.com>
2023-04-29 18:31:16 +0300

    Added basic authorization system

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 # url patterns for the base app
 urlpatterns = [
-    path('', views.MainView.as_view(), name='index_view'),
-    
+    path('', views.IndexView.as_view(), name='index_view'),
+    path('auth/login', views.LoginView.as_view(), name='login_view'),
+    path('auth/logout', views.logout_view, name='logout_view')
 ]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth import authenticate, login, logout
 
-
-class MainView(View):
+class IndexView(View):
     template_name = 'index.html'
 
     def get(self, request):
         context = {}
 
         return render(request, self.template_name, context)
+
+
+class LoginView(View):
+    template_name = 'base/login_view.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('index_view')
+
+        context = {}
+        
+        return render(request, self.template_name, context)
+    
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+            return redirect('index_view')
+        else:
+            return redirect('login_view')
+
+
+# FIXME: maybe this is a bad practice to mix class-base views and function-base views
+def logout_view(request):
+    logout(request)
+    return redirect('index_view')
