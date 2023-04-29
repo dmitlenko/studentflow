75bb2b2126e07b84799cb1ab5cd4cced99136005
Denys Mitlenko <softonka@gmail.com>
2023-04-29 19:37:48 +0300

    Added registration page and made all forms in a django way

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index_view'),
     path('auth/login', views.LoginView.as_view(), name='login_view'),
-    path('auth/logout', views.logout_view, name='logout_view')
+    path('auth/logout', views.logout_view, name='logout_view'),
+    path('auth/register', views.RegistrationView.as_view(), name='registration_view')
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from .forms import RegistrationForm
 
 class IndexView(View):
     template_name = 'index.html'
@@ -17,22 +20,51 @@ class LoginView(View):
     def get(self, request):
         if request.user.is_authenticated:
             return redirect('index_view')
-
-        context = {}
         
-        return render(request, self.template_name, context)
+        return render(request, self.template_name, {'form':AuthenticationForm})
+    
+    def post(self, request): 
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Authorization successful")
+
+                return redirect('index_view')
+        
+        messages.error(request, "Unsuccessful authentication. Invalid information.")
+
+        return redirect('login_view')
+    
+class RegistrationView(View):
+    template_name = 'base/registration_view.html'
+
+    def get(self, request, form:RegistrationForm=None):
+        if request.user.is_authenticated:
+            return redirect('index_view')
+        
+        if form is None:
+            form = RegistrationForm
+
+        return render(request, self.template_name, {'form':form})
     
     def post(self, request):
-        username = request.POST['username']
-        password = request.POST['password']
-        
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
+        form = RegistrationForm(data=request.POST)
 
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful")    
             return redirect('index_view')
-        else:
-            return redirect('login_view')
+        
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+
+        return self.get(request, form)
 
 
 # FIXME: maybe this is a bad practice to mix class-base views and function-base views
