1eb08adf1b2f13691a855a9bd28bd93bfb315eeb
dmitlenko <softonka@gmail.com>
2023-05-02 15:24:43 +0000

    Added chat group page

--- chat/urls.py
+++ chat/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='chat_home'),
+    path('<int:pk>', ChatGroupView.as_view(), name='chat'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import View
+from django.views.generic import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import ChatGroup, ChatGroupMessage
 
@@ -17,3 +18,26 @@ class HomeView(LoginRequiredMixin, View):
         return render(request, self.template_name, {
             'chats':chats
         })
+
+
+class ChatGroupView(LoginRequiredMixin, DetailView):
+    model = ChatGroup
+    template_name = 'chat/chat.html'
+    login_url = 'login'
+
+    def get_object(self, queryset=None):
+        obj = get_object_or_404(self.model, pk=self.kwargs['pk'])
+        return obj
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['messages'] = self.get_object().chatgroupmessage_set.all()
+        return context
+    
+    def dispatch(self, request, **kwargs):
+        obj = self.get_object()
+
+        if request.user == obj.creator or request.user in obj.participants.all():
+            return super().dispatch(request, **kwargs)
+        else:
+            return self.handle_no_permission()
