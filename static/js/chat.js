1701f2b76d166d4cf9570c43d0baa2296f472dd6
dmitlenko <softonka@gmail.com>
2023-05-02 20:44:28 +0300

    Created basic structure of chat back-end

--- static/js/chat.js
+++ static/js/chat.js
@@ -5,5 +5,24 @@ const chatSocket = new WebSocket(`ws://${window.location.host}/ws/chat/${roomId}
 chatSocket.onmessage = (e) => {
     const data = JSON.parse(e.data);
     document.querySelector('#chatLog').innerHTML += `<div>${data.message}</div>`;
-};
+}
 
+chatSocket.onclose = (e) => {
+    console.log('🙀🙀 Chat socket closed unexpectedly');
+}
+
+document.querySelector('#chat-message-input').focus();
+document.querySelector('#chat-message-input').onkeyup = (e) => {
+    if (e.keyCode === 13) {  
+        document.querySelector('#chat-message-submit').click();
+    }
+}
+
+document.querySelector('#chat-message-submit').onclick = (e) => {
+    const messageInputDom = document.querySelector('#chat-message-input');
+    const message = messageInputDom.value;
+    chatSocket.send(JSON.stringify({
+        'message': message
+    }));
+    messageInputDom.value = '';
+}
--- studentflow/asgi.py
+++ studentflow/asgi.py
@@ -1,11 +1,18 @@
+import chat.routing
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentflow.settings')
+django_asgi_application = get_asgi_application()
+
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application()
-
+    "http": django_asgi_application,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+    ),
 })
