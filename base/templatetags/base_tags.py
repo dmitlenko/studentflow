04229e03bd7170764ffeb9849665c4788b147644
dmitlenko <softonka@gmail.com>
2023-05-05 19:04:15 +0000

    Moved post card to filter

--- base/templatetags/base_tags.py
+++ base/templatetags/base_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from base.models import UserFollow, Post
+from django.template.loader import render_to_string
 
 register = template.Library()
 
@@ -10,3 +11,7 @@ def is_followed(user, follower):
 @register.filter('is_liked')
 def is_liked(post, user):
     return post.likes.filter(id=user.id).exists()
+
+@register.filter('post_card')
+def post_card(post, prefix=''):
+    return render_to_string('base/items/post_card.html', {'post': post, 'prefix':prefix})
