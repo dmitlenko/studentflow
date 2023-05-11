3d0befc3fe3e4e86d68a65c41ef426173bcf0705
dmitlenko <softonka@gmail.com>
2023-05-11 16:24:34 +0000

    made use of validate_file_size in chat app

--- chat/models.py
+++ chat/models.py
@@ -1,18 +1,13 @@
 from base.models import User
 from django.db import models
 from django.core.exceptions import ValidationError
+from base.validators import validate_file_size
 
 # Create your models here.
-class ChatGroup(models.Model):
-    def validate_image(fieldfile_obj):
-        filesize = fieldfile_obj.file.size
-        megabyte_limit = 5.0
-        if filesize > megabyte_limit*1024*1024:
-            raise ValidationError("Max file size is %sMB" % str(megabyte_limit))
-        
+class ChatGroup(models.Model):        
     creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     participants = models.ManyToManyField(User, verbose_name='Учасники', related_name='participants', blank=True)
-    image = models.ImageField('Фото', default='default_chatgroup.png', upload_to='images/groups', blank=True, validators=[validate_image])
+    image = models.ImageField('Фото', default='default_chatgroup.png', upload_to='images/groups', blank=True, validators=[validate_file_size])
     description = models.CharField('Опис', max_length=2000, blank=True)
     name = models.CharField('Назва', max_length=200)
     private = models.BooleanField(default=False)
