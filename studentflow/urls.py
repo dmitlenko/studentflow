62df3d9ef801e69a6979b524c40813ff3319d8b9
dmitlenko <softonka@gmail.com>
2023-05-18 22:12:41 +0000

    Added api to routes

--- studentflow/urls.py
+++ studentflow/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('base.urls')),
-    path('chat/', include('chat.urls'))
+    path('chat/', include('chat.urls')),
+    path('api/', include('api.urls'))
 ]
