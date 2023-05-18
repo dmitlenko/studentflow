02bedff2ecb2565a995a7e131da494426cb49b57
dmitlenko <softonka@gmail.com>
2023-05-18 22:12:34 +0000

    Some organization

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,24 +1,3 @@
-function getCookie(name) {
-    const cookieArr = document.cookie.split(';');
-    for (let i = 0; i < cookieArr.length; i++) {
-        const cookiePair = cookieArr[i].split('=');
-        if (name === cookiePair[0].trim()) {
-            return decodeURIComponent(cookiePair[1]);
-        }
-    }
-    return null;
-}
-
-function setCookie(name, value, days) {
-    let expires = '';
-    if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = `; expires=${date.toUTCString()}`;
-    }
-    document.cookie = `${name}=${encodeURIComponent(value)}${expires}; path=/`;
-}
-
 window.addEventListener('DOMContentLoaded', () => {
     const savedTheme = getCookie('theme');
     const themeList = document.querySelector('#themeList');
