5b5aa35704cc71d0d5531f6aec27d67c1a02942c
Denis Mitlenko <softonka@gmail.com>
2024-02-13 15:24:43 +0200

    Fix import order in asgi.py and settings.py

--- studentflow/project/asgi.py
+++ studentflow/project/asgi.py
@@ -1,16 +1,16 @@
-from studentflow.chat import routing
 import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from chat.middleware import TokenAuthMiddleware
 
 os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = 'true'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentflow.settings')
 
 django_asgi_application = get_asgi_application()
 
+from studentflow.chat.middleware import TokenAuthMiddleware
+from studentflow.chat import routing
 
 application = ProtocolTypeRouter({
     'http': django_asgi_application,
--- studentflow/project/settings.py
+++ studentflow/project/settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'base.middleware.TokenMiddleware',
+    'studentflow.base.middleware.TokenMiddleware',
 ]
 
 REST_FRAMEWORK = {
